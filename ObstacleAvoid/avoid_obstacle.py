31b06b81a8aff8d522d34876f6073e4de34bcf22
JakubBacik <jakub99.b@gmail.com>
2022-05-28 19:43:18 +0200

    Avoid Obstacle ver 1.0

--- ObstacleAvoid/avoid_obstacle.py
+++ ObstacleAvoid/avoid_obstacle.py
@@ -1,46 +1,78 @@
 #!/usr/bin/env python
-import rospy # Python library for ROS
-from sensor_msgs.msg import LaserScan #import library for lidar sensor
-from nav_msgs.msg import Odometry #import library for position and orientation data
+from re import A
+import rospy 
+from sensor_msgs.msg import LaserScan 
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
-class Circling(): #main class
+class Circling(): 
    
-    def __init__(self): #main function
+    def __init__(self): 
         global circle
-        circle = Twist() #create object of twist type  
-        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10) #publish message
-        self.sub = rospy.Subscriber("/spur/laser/scan", LaserScan, self.callback) #subscribe message 
-        self.sub = rospy.Subscriber("/odom", Odometry, self.odometry) #subscribe message
+        circle = Twist()  
+        self.flag = 0
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1) 
+        self.sub = rospy.Subscriber("/spur/laser/scan", LaserScan, self.callback)
+        self.sub = rospy.Subscriber("/odom", Odometry, self.odometry) 
 
-    def callback(self, msg): #function for obstacle avoidance
+    def callback(self, msg): 
         print('-------RECEIVING LIDAR SENSOR DATA-------')
-        print('Front: {}'.format(msg.ranges[0])) #lidar data for front side
-        print('Left:  {}'.format(msg.ranges[90])) #lidar data for left side
-        print('Right: {}'.format(msg.ranges[270])) #lidar data for right side
-        print('Back: {}'.format(msg.ranges[180])) #lidar data for back side
-      
-      	#Obstacle Avoidance
-        self.distance = 0.7
-        if msg.ranges[0] > self.distance and msg.ranges[15] > self.distance and msg.ranges[345] > self.distance: 
-        #when no any obstacle near detected
-            circle.linear.x = 0.5 # go (linear velocity)
-            circle.angular.z = 0.1 # rotate (angular velocity)
-            rospy.loginfo("Circling") #state situation constantly
-        else: #when an obstacle near detected
-            rospy.loginfo("An Obstacle Near Detected") #state case of detection
-            circle.linear.x = 0.0 # stop
-            circle.angular.z = 0.5 # rotate counter-clockwise
-            if msg.ranges[0] > self.distance and msg.ranges[15] > self.distance and msg.ranges[345] > self.distance and msg.ranges[45] > self.distance and msg.ranges[315] > self.distance:
-                #when no any obstacle near detected after rotation
-                circle.linear.x = 0.5 #go
-                circle.angular.z = 0.1 #rotate
-        self.pub.publish(circle) # publish the move object
+        print('Front:  {}'.format(min(min(msg.ranges[640:896]), 10))) 
+        print('RightFront: {}'.format(min(min(msg.ranges[522:523]), 10)))
+        print('Right: {}'.format(min(min(msg.ranges[502:503]), 10)))
+        print('Back: {}'.format(msg.ranges[256])) 
+        regions = {
+            'right':  min(min(msg.ranges[502:503]), 10),
+            'front':  min(min(msg.ranges[640:896]), 10),
+            #'left':   min(min(min(msg.ranges[0:12]), min(msg.ranges[955:1023])), 10),
+            'rightFront':  min(min(msg.ranges[522:523]), 10),
+            #'leftFront':   min(min(min(msg.ranges[0:12]), min(msg.ranges[955:1023])), 10),
+        }
+        self.take_action(regions)
 
-    def odometry(self, msg): #function for odometry
-        print(msg.pose.pose) #print position and orientation of turtlebot
+
+    def odometry(self, msg): 
+        print()#msg.pose.pose)
+
+    def take_action(self,regions):
+        threshold_dist = 0.9
+        linear_speed = -0.2
+        angular_speed = 0.8
+        
+        linear_x = 0
+        angular_z = 0
+        
+        state_description = ''
+        
+
+ 
+        if regions['front'] > threshold_dist:
+            state_description = 'No obstacle'
+            linear_x = linear_speed
+            angular_z = 0
+        elif regions['front'] < threshold_dist:
+            state_description = 'Front'
+            linear_x = 0
+            angular_z = angular_speed    
+  
+
+        if (abs(regions['rightFront'] - regions['right'])) > 0.00001 and regions['rightFront'] < regions['right'] and  regions['rightFront'] < 1.5:
+            state_description = 'rightFront less right'
+            linear_x = linear_speed
+            angular_z = 0.2
+        
+        if (abs(regions['rightFront'] - regions['right'])) > 0.00001 and regions['rightFront'] > regions['right'] and regions['right'] < 1.5:
+            state_description = 'rightFront more right'
+            linear_x = linear_speed
+            angular_z = -0.2
+        
+
+        rospy.loginfo(state_description)
+        circle.linear.x = linear_x
+        circle.angular.z = angular_z
+        self.pub.publish(circle)
 
 if __name__ == '__main__':
-    rospy.init_node('obstacle_avoidance_node') #initilize node
-    Circling() #run class
-    rospy.spin() #loop it   
+    rospy.init_node('obstacle_avoidance_node') 
+    Circling() 
+    rospy.spin()   
--- ObstacleAvoid/sim_ws/src/motion_plan/scripts/avoid_obstacle.py
+++ ObstacleAvoid/sim_ws/src/motion_plan/scripts/avoid_obstacle.py
@@ -10,20 +10,23 @@ class Circling():
     def __init__(self): 
         global circle
         circle = Twist()  
+        self.flag = 0
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1) 
         self.sub = rospy.Subscriber("/spur/laser/scan", LaserScan, self.callback)
         self.sub = rospy.Subscriber("/odom", Odometry, self.odometry) 
 
     def callback(self, msg): 
         print('-------RECEIVING LIDAR SENSOR DATA-------')
-        print('Front:  {}'.format(msg.ranges[748])) 
-        print('Left: {}'.format(msg.ranges[768]))
-        print('Right: {}'.format(msg.ranges[728]))
+        print('Front:  {}'.format(min(min(msg.ranges[640:896]), 10))) 
+        print('RightFront: {}'.format(min(min(msg.ranges[522:523]), 10)))
+        print('Right: {}'.format(min(min(msg.ranges[502:503]), 10)))
         print('Back: {}'.format(msg.ranges[256])) 
         regions = {
-            'right':  min(min(msg.ranges[500:580]), 10),
-            'front':  min(min(msg.ranges[581:956]), 10),
-            'left':   min(min(min(msg.ranges[0:12]), min(msg.ranges[955:1023])), 10),
+            'right':  min(min(msg.ranges[502:503]), 10),
+            'front':  min(min(msg.ranges[640:896]), 10),
+            #'left':   min(min(min(msg.ranges[0:12]), min(msg.ranges[955:1023])), 10),
+            'rightFront':  min(min(msg.ranges[522:523]), 10),
+            #'leftFront':   min(min(min(msg.ranges[0:12]), min(msg.ranges[955:1023])), 10),
         }
         self.take_action(regions)
 
@@ -33,49 +36,36 @@ class Circling():
 
     def take_action(self,regions):
         threshold_dist = 0.9
-        linear_speed = -1.2
-        angular_speed = -0.2
+        linear_speed = -0.2
+        angular_speed = 0.8
         
         linear_x = 0
         angular_z = 0
         
         state_description = ''
         
-        if regions['front'] > threshold_dist and regions['left'] > threshold_dist and regions['right'] > threshold_dist:
+
+ 
+        if regions['front'] > threshold_dist:
             state_description = 'No obstacle'
             linear_x = linear_speed
             angular_z = 0
-        elif regions['front'] < threshold_dist and regions['left'] < threshold_dist and regions['right'] < threshold_dist:
-            state_description = 'Front and left and right'
-            linear_x = -linear_speed
-            angular_z = angular_speed 
-        elif regions['front'] < threshold_dist and regions['left'] > threshold_dist and regions['right'] > threshold_dist:
+        elif regions['front'] < threshold_dist:
             state_description = 'Front'
             linear_x = 0
-            angular_z = angular_speed
-        elif regions['front'] > threshold_dist and regions['left'] > threshold_dist and regions['right'] < threshold_dist:
-            state_description = 'Right'
-            linear_x = 0
-            angular_z = -angular_speed
-        elif regions['front'] > threshold_dist and regions['left'] < threshold_dist and regions['right'] > threshold_dist:
-            state_description = 'Left'
-            linear_x = 0
-            angular_z = angular_speed
-        elif regions['front'] < threshold_dist and regions['left'] > threshold_dist and regions['right'] < threshold_dist:
-            state_description = 'Front and right'
-            linear_x = 0
-            angular_z = -angular_speed
-        elif regions['front'] < threshold_dist and regions['left'] < threshold_dist and regions['right'] > threshold_dist:
-            state_description = 'Front and left'
-            linear_x = 0
-            angular_z = angular_speed
-        elif regions['front'] > threshold_dist and regions['left'] < threshold_dist and regions['right'] < threshold_dist:
-            state_description = 'Left and right'
+            angular_z = angular_speed    
+  
+
+        if (abs(regions['rightFront'] - regions['right'])) > 0.00001 and regions['rightFront'] < regions['right'] and  regions['rightFront'] < 1.5:
+            state_description = 'rightFront less right'
             linear_x = linear_speed
-            angular_z = 0
-        else:
-            state_description = 'unknown case'
-            rospy.loginfo(regions)
+            angular_z = 0.2
+        
+        if (abs(regions['rightFront'] - regions['right'])) > 0.00001 and regions['rightFront'] > regions['right'] and regions['right'] < 1.5:
+            state_description = 'rightFront more right'
+            linear_x = linear_speed
+            angular_z = -0.2
+        
 
         rospy.loginfo(state_description)
         circle.linear.x = linear_x
